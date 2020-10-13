41b6ee2cc14779736fddf589719f515ca5302d7d
Tim Kotowski <tkotowski7@gmail.com>
2020-10-12 23:57:58 -0500

    jwt almost done

--- api/middleware/auth/auth.go
+++ api/middleware/auth/auth.go
@@ -6,26 +6,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"Go-JWT-Auth/api/cryption"
-	"Go-JWT-Auth/api/jwtgenerate"
 	"Go-JWT-Auth/models"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/chi"
 	jsoniter "github.com/json-iterator/go"
-	"github.com/volatiletech/sqlboiler/queries"
+	"github.com/volatiletech/sqlboiler/v4/queries"
 	"golang.org/x/crypto/bcrypt"
 )
 
 var (
-	db   *sql.DB
-	json = jsoniter.ConfigFastest
-	ctx  = context.Background()
+	db     *sql.DB
+	json   = jsoniter.ConfigFastest
+	ctx    = context.Background()
+	jwtKey = []byte("my_secret_key")
 )
 
 func New(db *sql.DB, r *chi.Mux) {
 	r.Post("/api/v1/signup", userSignUp(db))
 	r.Post("/api/v1/login", userLogin(db))
+	r.Post("/api/v1/welcome", userWelcome(db))
+}
+
+// Create a struct that will be encoded to a JWT.
+// We add jwt.StandardClaims as an embedded type, to provide fields like expiry time
+type Claims struct {
+	Email string `boil:"email" json:"email" toml:"email" yaml:"email"`
+	jwt.StandardClaims
 }
 
 //Above userSignup function is doing the user registration part. Here response body’s json
@@ -40,7 +50,7 @@ func userSignUp(db *sql.DB) http.HandlerFunc {
 		user.Password = cryption.GetHash([]byte(user.Password))
 		err := queries.Raw(`INSERT INTO users (firstname, lastname, email, password) VALUES ($1, $2, $3, $4) RETURNING id`,
 			user.Firstname, user.Lastname, user.Email, user.Password).Bind(ctx, db, user)
-		// err := user.Insert(ctx, db, boiler.Infer()) //TODO
+		// err := user.Insert(ctx, db, boil.Infer())
 		if err != nil {
 			log.Fatalf("\n err in q %v", err)
 			return
@@ -84,10 +94,86 @@ func userLogin(db *sql.DB) http.HandlerFunc {
 		if passErr != nil {
 			log.Fatalf("error in comapring passwords %v", passErr)
 		}
-		jwtToken, err := jwtgenerate.GenerateJWT()
-		if passErr != nil {
-			log.Fatalf("error in JWT token generation %v", err)
+		// Declare the expiration time of the token
+		// here, we have kept it as 5 minutes
+		expirationTime := time.Now().Add(5 * time.Minute)
+		// Create the JWT claims, which includes the username and expiry time
+		claims := &Claims{
+			Email: dbUser.Email,
+			StandardClaims: jwt.StandardClaims{
+				// In JWT, the expiry time is expressed as unix milliseconds
+				ExpiresAt: expirationTime.Unix(),
+			},
 		}
-		w.Write([]byte(`{"token": "` + jwtToken + `"}`))
+
+		// Declare the token with the algorithm used for signing, and the claims
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		// Create the JWT string
+		tokenString, err := token.SignedString(jwtKey)
+		if err != nil {
+			// If there is an error in creating the JWT return an internal server error
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		// Finally, we set the client cookie for "token" as the JWT we just generated
+		// we also set an expiry time which is the same as the token itself
+		http.SetCookie(w, &http.Cookie{
+			Name:    "token",
+			Value:   tokenString,
+			Expires: expirationTime,
+		})
+
+	}
+}
+
+func userWelcome(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c, err := r.Cookie("token")
+		if err != nil {
+			if err == http.ErrNoCookie {
+				// If the cookie is not set, return an unauthorized status
+				log.Fatalf("err no cookie found %v", err)
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			// For any other type of error, return a bad request status
+			log.Fatalf("err bad requesr no cookie found %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// Get the JWT string from the cookie
+		tknStr := c.Value
+
+		// Initialize a new instance of `Claims`
+		claims := &Claims{}
+
+		// Parse the JWT string and store the result in `claims`.
+		// Note that we are passing the key in this method as well. This method will return an error
+		// if the token is invalid (if it has expired according to the expiry time we set on sign in),
+		// or if the signature does not match
+		tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				log.Fatalf("err invalid signature %v", err)
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			log.Fatalf("badrequest %v", err)
+			return
+		}
+		if !tkn.Valid {
+			log.Fatalf("no token or invalid token %v", err)
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		// Finally, return the welcome message to the user, along with their
+		// username given in the token
+		w.Write([]byte(fmt.Sprintf("welcome %s", claims.Email)))
 	}
 }
--- go.mod
+++ go.mod
@@ -8,12 +8,10 @@ require (
 	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/json-iterator/go v1.1.10
 	github.com/kat-co/vala v0.0.0-20170210184112-42e1d8b61f12
-	github.com/lib/pq v1.8.0
-	github.com/spf13/viper v1.7.1
-	github.com/volatiletech/null v8.0.0+incompatible // indirect
+	github.com/lib/pq v1.2.1-0.20191011153232-f91d3411e481
+	github.com/spf13/viper v1.6.3
 	github.com/volatiletech/randomize v0.0.1
-	github.com/volatiletech/sqlboiler v3.7.1+incompatible
 	github.com/volatiletech/sqlboiler/v4 v4.2.0
 	github.com/volatiletech/strmangle v0.0.1
-	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0
+	golang.org/x/crypto v0.0.0-20201012173705-84dcc777aaee
 )
